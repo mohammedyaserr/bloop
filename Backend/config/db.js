a8399f8eda385f5f4116d37ff864bec80fd1d5ee
mohammedyaserr <yasermohammed687@gmail.com>
2026-05-31 00:47:48 +0530

    Add TiDB SSL support

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,13 +1,16 @@
 import mysql from 'mysql2'
 import dotenv from 'dotenv'
 
-// Load environment variables from .env file
 dotenv.config()
 
 export const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'root',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE || 'bloop'
+    database: process.env.DB_DATABASE,
+
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
